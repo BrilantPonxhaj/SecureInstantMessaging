b294634bc139cd49f40afaff99b130597c51463a
clirimtarcitaku <clirimtar.citaku@student.uni-pr.edu>
2024-05-19 21:30:57 +0200

    sql fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.datasecurity4 {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
 
     opens com.example.datasecurity4 to javafx.fxml;
     exports app;
