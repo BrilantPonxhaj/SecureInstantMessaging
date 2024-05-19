24653564c94961d4915b81dea176a0344c88604e
BrilantPonxhaj <brilant.ponxhaj@student.uni-pr.edu>
2024-05-19 02:13:39 +0200

    Fixed the server

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.datasecurity4 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.datasecurity4 to javafx.fxml;
+    exports com.example.datasecurity4;
+}
