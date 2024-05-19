629d5aa41965caa23eba1fb38ee846cccdcc6513
Brikenda-Zogaj <116032682+Brikenda-Zogaj@users.noreply.github.com>
2024-05-19 10:39:56 +0200

    Some changes

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.knk2324 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires org.bouncycastle.provider;
+
+
+
+    exports app;
+
+
+
+
+}
