b6ca4e575e8c32e5baabe8bf04053ca517f5c927
BrinetaUka <brineta.uka@student.uni-pr.edu>
2024-05-19 13:09:28 +0200

    Update SIMPGUI.java

--- src/main/java/app/SIMPGUI.java
+++ src/main/java/app/SIMPGUI.java
@@ -51,4 +51,54 @@ public class SIMPGUI extends Application {
         launch(args);
     }
 
+
+    
+
+
+    class MessageReceiver implements Runnable {
+        private DataInputStream input;
+        private SecretKeySpec aesKey;
+        private TextArea messageArea;
+        private String username;
+
+        public MessageReceiver(DataInputStream input, SecretKeySpec aesKey, TextArea messageArea, String username) {
+            this.input = input;
+            this.aesKey = aesKey;
+            this.messageArea = messageArea;
+            this.username = username;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    int length = input.readInt();
+                    if (length > 0) {
+                        byte[] encryptedMessage = new byte[length];
+                        input.readFully(encryptedMessage, 0, encryptedMessage.length);
+                        byte[] iv = new byte[12];
+                        input.readFully(iv);
+
+                        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+                        GCMParameterSpec spec = new GCMParameterSpec(128, iv);
+                        cipher.init(Cipher.DECRYPT_MODE, aesKey, spec);
+                        byte[] decryptedMessage = cipher.doFinal(encryptedMessage);
+
+                        String message = new String(decryptedMessage);
+                        Platform.runLater(() -> messageArea.appendText(message + "\n"));
+
+                        // Save the received message to the database
+                        String[] messageParts = message.split(": ", 2);
+                        if (messageParts.length == 2) {
+                            String sender = messageParts[0];
+                            String content = messageParts[1];
+                            SecureChat.storeMessage(sender, username, content);
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
